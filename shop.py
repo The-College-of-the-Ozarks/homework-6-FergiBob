8d87023bfefc27390380c841b774f4767cb85d38
FergiBob <112025360+FergiBob@users.noreply.github.com>
2022-10-24 14:50:04 -0500

    Update shop.py

--- shop.py
+++ shop.py
@@ -1,6 +1,11 @@
 # shop.py
 #
-# 
+#
+
+
+
+
+#file import prompt 
 while True:
     f = input('Please enter the file name of the product list: ')
     try:
@@ -9,7 +14,21 @@ while True:
         
     except:
         print('Invalid file name. Please try again.')
+
+
+#define and sort dictionaries
+BuyDict = {}
+SellDict = {}
+
+for line in c:
+    
+    line = line.split('\t')
+    if line[0] == 'Buy':
+        BuyDict[line[1]]=line[2].replace('\n','')
+    elif line[0] == 'Sell':
+        SellDict[line[1]]=line[2].replace('\n','')
         
+#menu                
 while True:
     print('-------------------- Menu --------------------')
     print(' [1] Print Buy List')
@@ -22,6 +41,17 @@ while True:
     print('*Enter the number of the desired option*\n')
     menuopt = input()
     
-    if menuopt == 1:
-    
-    
+    if menuopt == '1':
+        print(f'Item                    Price')
+        for key in BuyDict:
+            print(f'{'')
+
+
+
+
+
+    elif menuopt == '7':
+        break
+    else:
+        print('\nPlease enter a valid option\n')
+c.close
